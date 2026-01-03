464c0bff5a7cf95249352418e0045b534fedafa6
zuvy <zuvanich@gmail.com>
2026-01-03 14:14:52 -0600

    Worknig elementary page.

--- web_root/scripts/components/registration_count/controllers/index.js
+++ web_root/scripts/components/registration_count/controllers/index.js
@@ -1,7 +1,8 @@
 'use strict';
 define(function(require) {
     // Require all controllers at this level
-    require('components/registration_count/controllers/elemctrl');
+    require('components/registration_count/controllers/distctrl');
+    require('components/registration_count/controllers/elemregctrl');
     require('components/registration_count/controllers/settingsctrl');
     require('components/registration_count/controllers/testctrl');
 });
--- web_root/scripts/components/registration_count/services/index.js
+++ web_root/scripts/components/registration_count/services/index.js
@@ -1,5 +1,5 @@
 'use strict';
 define(function(require) {
     // Require all services at this level
-    require('components/registration_count/services/elem_data');
+    require('components/registration_count/services/dist_data');
 });
